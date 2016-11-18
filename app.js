47fc8be23c22130affa2106d7d26f4908f06aa9e
Shav <shavenchew@yahoo.com>
2016-11-18 13:37:14 -0800

    'Added Body-parser and a home route'

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express'),
     mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
     app = express();
 
+app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+  res.render('index');
+});
 
-
+app.post('/imageSearch', function(req, res){
+  app.send('Success');
+});
 
 app.listen('5000', function(){
   console.log('Listening on 5000');
