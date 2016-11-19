84a12ce436b5518b110eede5688b3d8b3ce10035
Shav <shavenchew@yahoo.com>
2016-11-18 16:21:29 -0800

    'Figuring out a rendering inside a for loop'

--- app.js
+++ app.js
@@ -27,30 +27,32 @@ app.post('/imageSearch', function(req, res){
   var search = req.body.searchValue;
   console.log(search);
 
+  var emptyArr = [];
+
   images.create({
     searchTerm : search,
     when : new Date()
   });
 
   lookFor.images(search,
-    {top: 5,
-    image: 20400
+    {
+     top: 5,
+     image: 20400
     },
     function(err, results){
     if(err){
       console.log(err);
     } else {
       console.log(results);
-
       for(var i = 0 ; i < results.length; i ++){
-        res.json({
-          imageURL: results[i].url,
+        emptyArr.push({
+          imageURL :results[i].url,
           altText : results[i].title,
-          pageURL : results[i].sourceUrl
+          sourceURL : results[i].sourceurl
         });
+       }
       }
-    }
-  });
+    });
 });
 
 app.get('/imageSearch/latest', function(req, res){
@@ -58,7 +60,7 @@ app.get('/imageSearch/latest', function(req, res){
     if(err){
       console.log(err);
     } else{
-      res.send(results);
+      res.json(results);
     }
   });
 });
